3a7d762e2f576b95e52c71ab479d135d9e40cc53
dweih <dave_weih@hotmail.com>
2016-06-16 22:45:46 -0700

    This test runs a cyborg / cyborg game

--- test.py
+++ test.py
@@ -1,12 +1,19 @@
 execfile('cyborg.py')
 execfile('chess_game.py')
-#execfile('BetterProbScore.py')
+execfile('BetterProbScore.py')
+execfile('New Allocator.py')
 
 #b = chess.Board()
 
-c = cyborg( boardScorer=pigglyv3, pointsPerMove= 100)#, probScorer = Betterprobscore )
-#c1 = cyborg( boardScorer=pigglyv3, pointsPerMove= 10 )
+c = cyborg( boardScorer=pigglyv3, pointsPerMove= 10, pointAllocator=PointAllocator)#, probScorer = Betterprobscore )
+c1 = cyborg( boardScorer=pigglyv3, pointsPerMove= 100, color=chess.BLACK )
 
+c.Build(1)
+c1.Build(1)
+
+#c1.acceptMove(chess.Move.from_uci('e2e4'))
+
+#c1.Build(1)
 
 #c.expandNode(c.current_board)
 
@@ -18,9 +25,6 @@ c = cyborg( boardScorer=pigglyv3, pointsPerMove= 100)#, probScorer = Betterprobs
 #c.chooseMove()
 
 
-#Expand Node is creating leaves that don't include history - all just moves from starting board
-
-
 #c.expandNode(c.current_board)
 #c.Score()
 #c.Build( 5 )
@@ -28,21 +32,10 @@ c = cyborg( boardScorer=pigglyv3, pointsPerMove= 100)#, probScorer = Betterprobs
 
 #c.chooseMove( g.board )
 
-# Status - just fixed error where all nodes were added to the root
-# Seems like some things were only working because of it...
-
-#c.Score()
 
 #g = game(white='naivePigglyv2', black='naivePigglyv3' )
-#g = game(white='c.chooseMove', black='c1.chooseMove', white_apply=c.acceptMove, black_apply=c1.acceptMove)
-g = game(white='c.chooseMove', black='naivePigglyv3', white_apply=c.acceptMove)
-#g = game(black='c.chooseMove', white='naivePigglyv3', black_apply=c.acceptMove)
+g = game(white='c.chooseMove', black='c1.chooseMove', white_apply=c.acceptMove, black_apply=c1.acceptMove)
+#g = game(white='naivePigglyv3', black='c1.chooseMove', black_apply=c1.acceptMove)
 
 g.play(pause = False)
 
-def listMoves(c):
-    moves = c.getScoredMoves( c.current_board )
-    sortedMoves = sorted(moves, key=itemgetter(1), reverse=True)
-    for m in sortedMoves:
-        print m[0], '  ', m[1]
-    return
