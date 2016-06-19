1b73b85ce9c5a059c260fed0353fa7f8fe9af77c
dweih <dave_weih@hotmail.com>
2016-06-18 22:42:47 -0700

    Modified cyborgs to use pigglyv4 (the white=+ version) so pigglyv3 could still be the naive-friendly version

--- test.py
+++ test.py
@@ -2,17 +2,16 @@ execfile('cyborg.py')
 execfile('chess_game.py')
 execfile('BetterProbScore.py')
 execfile('New Allocator.py')
+execfile('Cheryl.py')
 
 #b = chess.Board()
 
-c = cyborg( boardScorer=pigglyv3, pointsPerMove= 10, pointAllocator=PointAllocator)#, probScorer = Betterprobscore )
-c1 = cyborg( boardScorer=pigglyv3, pointsPerMove= 100, color=chess.BLACK )
+c = cyborg( boardScorer=pigglyv4, pointsPerMove= 50, pointAllocator=PointAllocator)#, probScorer = Betterprobscore )
+c1 = cyborg( boardScorer=pigglyv4, pointsPerMove= 50, color=chess.BLACK, pointAllocator=PointAllocator )
 
-c.Build(1)
-c1.Build(1)
-
+#c.Build(10)
+#c1.Build(1)
 #c1.acceptMove(chess.Move.from_uci('e2e4'))
-
 #c1.Build(1)
 
 #c.expandNode(c.current_board)
@@ -27,14 +26,20 @@ c1.Build(1)
 
 #c.expandNode(c.current_board)
 #c.Score()
+#c.Build( 1 )
+#c.acceptMove( chess.Move.from_uci('e2e4'))
+#c.Build(1)
 #c.Build( 5 )
+#c.Build( 5 )
+#c1.printGraph(depth=5)
 #c.getScoredMoves()
 
 #c.chooseMove( g.board )
 
 
 #g = game(white='naivePigglyv2', black='naivePigglyv3' )
-g = game(white='c.chooseMove', black='c1.chooseMove', white_apply=c.acceptMove, black_apply=c1.acceptMove)
+#g = game(white='c.chooseMove', black='c1.chooseMove', white_apply=c.acceptMove, black_apply=c1.acceptMove)
+g = game(white='c.chooseMove', black='naivePigglyv3', white_apply=c.acceptMove)
 #g = game(white='naivePigglyv3', black='c1.chooseMove', black_apply=c1.acceptMove)
 
 g.play(pause = False)
